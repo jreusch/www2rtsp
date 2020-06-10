e91ea6d86103c75a5cf1447b451ad19882cdd76e
Jan Reusch <jan@jreusch.de>
2020-06-11 00:21:55 +0200

    Add automation flag to not show remote control warning

--- src/WWW2RTSP/offscreen_browser.py
+++ src/WWW2RTSP/offscreen_browser.py
@@ -74,6 +74,7 @@ def get_chromium_options(width: int, height: int):
     options.add_argument("--noerrdialogs")
     options.add_argument("--incognito")
     options.add_argument("--window-size={},{}".format(width, height))
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
     return options
 
 # only used for dev purposes
